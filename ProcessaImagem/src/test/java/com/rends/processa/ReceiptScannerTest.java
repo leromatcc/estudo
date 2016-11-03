73e9ff2dcc76ec44adf6ad2179e57127ba157669
leroma.tcc <leroma.tcc@gmail.com>
2016-11-03 05:51:46 -0200

    Criação do segundo caso de teste

--- ProcessaImagem/src/test/java/com/rends/processa/ReceiptScannerTest.java
+++ ProcessaImagem/src/test/java/com/rends/processa/ReceiptScannerTest.java
@@ -1,31 +1,19 @@
-package com.rends.processa.ProcessaImagem;
+package com.rends.processa;
 
-import junit.framework.Test;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
 public class ReceiptScannerTest extends TestCase {
-
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( ReceiptScannerTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 
 	public void testReconhecimento(){
 		ReceiptScanner rcpt = new ReceiptScannerImpl();
 		rcpt.getTextFromReceiptImage("C:/tcc/workspace_estudo/ProcessaImagem/src/test/resources/images/receipt.jpg");
+        assertTrue( true );
+
+	}
+	
+	public void testReconhecimentoB(){
+		ReceiptScanner rcpt = new ReceiptScannerImpl();
+		rcpt.getTextFromReceiptImage("C:/tcc/workspace_estudo/ProcessaImagem/src/test/resources/images/receipt-b.jpg");
         assertTrue( true );
 
 	}
